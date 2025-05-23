d9ae70040b622f96cfe54f1a71eefb8f38ab3df6
Samadar1 <urapol18@gmail.com>
2025-05-23 17:47:09 +0300

    add: get email by username

--- src/main/java/com/example/auth_service/controller/AuthController.java
+++ src/main/java/com/example/auth_service/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.auth_service.controller;
 
 import com.example.auth_service.dto.*;
+import com.example.auth_service.repository.UserRepository;
 import com.example.auth_service.service.AuthenticationService;
 import com.example.auth_service.service.JwtService;
 import com.example.auth_service.service.UserService;
@@ -8,11 +9,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,9 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationService authenticationService;
     private final JwtService jwtService;
-    public AuthController(AuthenticationService authenticationService, JwtService jwtService) {
+    private final UserRepository userRepository;
+    public AuthController(AuthenticationService authenticationService, JwtService jwtService, UserRepository userRepository) {
         this.authenticationService = authenticationService;
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
     }
 
     @Operation(summary = "Регистрация пользователя")
@@ -48,4 +49,12 @@ public class AuthController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @GetMapping("/{username}/email")
+    public ResponseEntity<String> getEmailByUsername(@PathVariable String username) {
+        return userRepository.findByUsername(username)
+                .map(user -> new ResponseEntity<>(user.getEmail(), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND));
+    }
+
 }
